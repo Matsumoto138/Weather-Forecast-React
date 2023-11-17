6f93595dd84d446b9f9b4ed2a972aa8aa05c9e52
Mobimenti <139997876+Mobimenti@users.noreply.github.com>
2023-11-17 23:13:15 +0300

    Add Some Animations

--- src/Components/WeeklyForecast.js
+++ src/Components/WeeklyForecast.js
@@ -1,29 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import '../Style/WeeklyForecast.css'
-import ThunderIcon from '../Images/ThunderIcon.svg'
-import SunnyIcon from '../Images/SunnyIcon.svg'
-import SunnyCloud from '../Images/SunnyCloud.svg'
-import SunnyRainIcon from '../Images/SunnyRainIcon.png'
-import RainyIcon from '../Images/RainyIcon.svg'
+import React, { useState, useEffect } from 'react';
+import '../Style/WeeklyForecast.css';
+import ThunderIcon from '../Images/ThunderIcon.svg';
+import SunnyIcon from '../Images/SunnyIcon.svg';
+import SunnyCloud from '../Images/SunnyCloud.svg';
+import SunnyRainIcon from '../Images/SunnyRainIcon.png';
+import RainyIcon from '../Images/RainyIcon.svg';
 
 const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 export default function WeeklyForecast({ data }) {
+  const [selectedWeekIndex, setSelectedWeekIndex] = useState(1);
+  const [animationKey, setAnimationKey] = useState(0);
 
-  const [selectedWeekIndex, setSelectedWeekIndex] = React.useState(1);
-
-
-  if (!data){
+  if (!data) {
     return <>Loading...</>;
-  } 
+  }
 
   const prevClick = () => {
     setSelectedWeekIndex((i) => Math.max(i - 1, 0));
-  }
+    setAnimationKey((prevKey) => prevKey + 1);
+  };
 
   const nextClick = () => {
     setSelectedWeekIndex((i) => Math.min(i + 1, 2));
-  }
+    setAnimationKey((prevKey) => prevKey + 1);
+  };
 
   const selectIcon = (type) => {
     switch (type) {
@@ -38,28 +39,25 @@ export default function WeeklyForecast({ data }) {
       case "sunny cloudy":
         return SunnyCloud;
       default:
-        return null; // or a default icon
+        return null;
     }
-  }
+  };
 
   const selectedWeekKey = ["previous", "current", "next"][selectedWeekIndex];
   const selectedWeekData = data[selectedWeekKey];
-  
-  
 
   return (
     <div className="WeeklyForecast">
       <div className="Prev-Btn">
         <button onClick={prevClick}>&lt;</button>
       </div>
-      <div className="Forecast-List">
-
-        {days.map((day) => (
-          <div className='List' key={day}>
+      <div className="Forecast-List" key={animationKey}>
+        {days.map((day, index) => (
+          <div className='List' key={day} style={{ animationDelay: `${index * 100}ms` }}>
             <p className='Day'>{day}</p>
             {selectIcon(selectedWeekData[day].type) && (
-            <img src={selectIcon(selectedWeekData[day].type)} alt="" />
-          )}
+              <img src={selectIcon(selectedWeekData[day].type)} alt="" />
+            )}
             <p>{selectedWeekData[day].degree}°</p>
           </div>
         ))}
